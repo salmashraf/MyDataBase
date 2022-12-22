0183f8910a783fb6c53c51976cc6335d8228d257
salmashraf <selma.ashraf@yahoo.com>
2022-12-22 16:53:18 +0200

    adding table features

--- mydatabase/proj.sh
+++ mydatabase/proj.sh
@@ -3,18 +3,48 @@
 select i in CreateTable ListTables DropTable Insert_into_Table Select_from_Table Delete_from_Table UpdateTable
 do
 case $i in 
-  CreateTable)
+ CreateTable)
+      declare -i j=1
+      read -p "Please Enter Number of columns: " number
+      while (( $j < number+1 ))
+      do
+      read -p "Please Enter $j Argument: " argument
+      read -p "Please Enter its datatype: " datatype
+       ((j = $j + 1))
+      done
    ;; 
-  ListTables)
+ ListTables)
    ls -F | grep -v "/" 
+   echo " Helloo "
    ;; 
-  DropTable)
+ DropTable)
+   rm $name
    ;; 
-  Insert_into_Table)
+ Insert_into_Table)
+   echo " hello "
    ;; 
-  Select_from_Table)
+ Select_from_Table)
+   echo " hello "
    ;; 
-  Delete_from_Table)
-   ;; 
-  UpdateTable)
-   ;; 
+ Delete_from_Table)
+   read -p "Do you want to delete a row or column ? " answer
+     if [[ $answer = [Rr]ow ]];then
+        read -p "Please Enter the Table name: " name
+        read -p "Please Enter the row number: " r
+        ls -R /$name | grep "$r"
+     elif [[ $answer = [Cc]olumn ]];then
+        read -p "Please Enter the Table name: " name
+        read -p "Please Enter the column number: " c
+          cut -d : -f $c ~/$name
+    else 
+      echo " Please write wether R(r)ow or C(c)olumn"
+    fi
+  ;; 
+   
+ UpdateTable)
+ echo " hello " 
+   ;;
+ *)
+   break 
+esac
+done
